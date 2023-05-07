ba0a274e535e5fc2651119d8bd19900247be4dbf
Elisee-des <esabidani@gmail.com>
2023-05-07 16:31:19 +0000

    Gestion des importes

--- src/pages/Public/PublicRouter.js
+++ src/pages/Public/PublicRouter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Layout from '@/pages/Public/Layout';
+// import Layout from '@/pages/Public/Layout';
+// import Home from '@/pages/Public/Home';
+// import Service from '@/pages/Public/Service';
+// import Contact from '@/pages/Public/Contact';
+import { Layout, Home, Service, Contact } from '@/pages/Public';
 import Error from '@/_utiles/Error';
-import Home from '@/pages/Public/Home';
-import Service from '@/pages/Public/Service';
-import Contact from '@/pages/Public/Contact';
+
 
 const PublicRoute = () => {
     return (
