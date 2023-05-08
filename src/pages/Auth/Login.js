09cab01bccec7330ee170c7135efa75846393db4
Elisee-des <esabidani@gmail.com>
2023-05-08 03:25:15 +0000

    Branchement formulaire login

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './auth.css';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     let navigate = useNavigate();
+
+    const [login, setLogin] = useState('Elisee');
+    const [password, setPassword] = useState('Password');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Mon formulaire");
+        console.log(login, password);
+    }
+
     const handlRetour = () => {
         navigate('../home');
     }
@@ -12,14 +22,14 @@ const Login = () => {
     return (
         <div className='Login'>
             <h3 className='inscipt-form'>Formulaire de connexion</h3>
-            <form action="">
+            <form action="" onSubmit={onSubmit}>
                 <div className="group">
                     <label htmlFor="login">Identification</label>
-                    <input type="text" name='login' />
+                    <input type="text" name='login' value={login} onChange={(e) => setLogin(e.target.value)} />
                 </div>
                 <div className="group">
                     <label htmlFor="password">Mot de passe</label>
-                    <input type="text" name='password' />
+                    <input type="text" name='password' value={password} onChange={e=>setPassword(e.target.value)} />
                 </div>
                 <div className="group">
                     <button>Connexion</button>
