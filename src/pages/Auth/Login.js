2758eec6423efad1185741078213a375ce367968
Elisee-des <esabidani@gmail.com>
2023-05-08 03:48:47 +0000

    Recuperation des donner du formaulaire avec USESTATE

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -6,13 +6,23 @@ const Login = () => {
 
     let navigate = useNavigate();
 
-    const [login, setLogin] = useState('Elisee');
-    const [password, setPassword] = useState('Password');
+    const [credentials, setCredential] = useState({
+        login: 'Elisee',
+        password: '0190'
+    });
+
+    const onChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        setCredential({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log("Mon formulaire");
-        console.log(login, password);
+        console.log(credentials);
     }
 
     const handlRetour = () => {
@@ -25,11 +35,11 @@ const Login = () => {
             <form action="" onSubmit={onSubmit}>
                 <div className="group">
                     <label htmlFor="login">Identification</label>
-                    <input type="text" name='login' value={login} onChange={(e) => setLogin(e.target.value)} />
+                    <input type="text" name='login' value={credentials.login} onChange={onChange} />
                 </div>
                 <div className="group">
                     <label htmlFor="password">Mot de passe</label>
-                    <input type="text" name='password' value={password} onChange={e=>setPassword(e.target.value)} />
+                    <input type="text" name='password' value={credentials.password} onChange={onChange} />
                 </div>
                 <div className="group">
                     <button>Connexion</button>
